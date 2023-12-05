14d887f1b06befd12957d8948f1722c5a482e07a
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-06 00:05:21 +0900

    Bug fix: invalid response

--- backend/controllers/event.ts
+++ backend/controllers/event.ts
@@ -1,5 +1,6 @@
+import { Request, Response } from 'express'
 import event from '../config/event'
 
-export const getInfo = (req: any, res: any) => {
-    return event
+export const getInfo = (req: Request, res: Response) => {
+    return res.json(event)
 }
