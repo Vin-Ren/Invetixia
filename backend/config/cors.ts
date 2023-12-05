a9042efca28bce0117716d3ab59995b5a95f733d
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-05 23:44:42 +0900

    Moved cors options to config folder

---
+++ backend/config/cors.ts
@@ -0,0 +1,14 @@
+import { env } from "process"
+
+let originList: any[] = ['*'] // allow all by default
+if (env.ROOT_DOMAIN !== undefined) {
+    const subdomainRe = new RegExp(`\.${env.ROOT_DOMAIN.replace(".", "\\.")}$`)
+    originList = [`http://${env.ROOT_DOMAIN}`, `https://${env.ROOT_DOMAIN}`, subdomainRe]
+}
+
+const corsConfig = { // ref: https://expressjs.com/en/resources/middleware/cors.html
+    origin: originList, 
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+export default corsConfig
