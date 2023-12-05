b67fae6222b65ca28f5fef0c85c3736639ca1319
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-05 23:45:06 +0900

    Added event route

--- backend/app.ts
+++ backend/app.ts
@@ -2,10 +2,13 @@ import express from "express";
 import cors from 'cors';
 import compression from 'compression'
 
+import eventRouter from './routes/event';
 import corsConfig from './config/cors';
 
 const app = express()
 app.use(cors(corsConfig))
 app.use(compression())
+
+app.use('/event', eventRouter)
 
 export default app
