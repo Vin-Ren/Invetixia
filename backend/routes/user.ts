f6a221b3df225bb638c5fde051cd44f64a568dc3
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-06 15:27:01 +0900

    Bug fixes and additional routes for user

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,17 +1,21 @@
 import { Router } from "express";
 import { refreshToken } from "../controllers/refreshToken";
-import { getRoles, getUsers, create, login, changePassword, logout } from "../controllers/user";
+import { getRoles, getUsers, getUser, create, login, changePassword, update, logout } from "../controllers/user";
 import verifyToken from "../middlewares/verifyToken";
 
 
 const userRouter = Router({caseSensitive:true, mergeParams: true})
 
 userRouter.get('/', verifyToken, getUsers)
-userRouter.get('/roles', getRoles)
+userRouter.get('/info/:id', verifyToken, getUser)
+
 userRouter.post('/create', verifyToken, create)
 userRouter.post('/login', verifyToken, login)
 userRouter.post('/changePassword', verifyToken, changePassword)
+userRouter.patch('/update', verifyToken, update)
 userRouter.post('/logout', verifyToken, logout)
+
 userRouter.get('/refreshToken', refreshToken)
+userRouter.get('/roles', getRoles)
 
 export default userRouter
