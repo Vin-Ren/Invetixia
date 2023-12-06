ee6b70f8a216a396df6a53bdebf82b528d1e8375
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-06 18:46:35 +0900

    Added optional query logging for prismaClient

--- backend/database.ts
+++ backend/database.ts
@@ -1,6 +1,7 @@
+import { env } from "process";
 import { PrismaClient } from "@prisma/client";
 
-export const prismaClient = new PrismaClient();
+const { ENABLE_QUERY_LOGGING } = env;
 
 export const userRole = { 
     SUPER_ADMIN: 0b1000, 
@@ -8,3 +9,18 @@ export const userRole = {
     ORGANISATION_MANAGER: 0b0010, 
     OBSERVER: 0b0001 
 }
+
+function createPrismaClient() {
+    if (ENABLE_QUERY_LOGGING) {
+        const prismaClient = new PrismaClient({ 
+            log:[ { level:'query', emit: 'event' } ]
+        })
+        prismaClient.$on('query', (event) => console.log({event}))
+        return prismaClient
+    } else {
+        const prismaClient = new PrismaClient()
+        return prismaClient
+    }
+}
+
+export const prismaClient = createPrismaClient()
