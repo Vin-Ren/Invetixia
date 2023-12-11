ce61e13f826a42bf343f9c55edd6a7fc724533e3
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2023-12-11 17:14:38 +0900

    Added test cases for organisation manager

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -61,7 +61,7 @@ class TestAdmin:
         self.su_session.logout()
     
     def test_get_users(self):
-        res = self.session.request_path("GET", '/user/roles')
+        res = self.session.request_path("GET", '/user')
         assert (res.ok)
     
     def test_get_super_user(self):
@@ -82,6 +82,67 @@ class TestAdmin:
         assert (res.ok)
     
     @pytest.mark.xfail(reason="Admin cannot modify superadmin")
+    def test_change_super_user_role(self):
+        updated_data = self.su_session.info.copy();
+        updated_data['role'] = 1
+        res = self.session.request_path("PATCH", '/user/update', json=updated_data)
+        assert (res.ok)
+    
+    @pytest.mark.xfail(reason="Updates on a user can only be done by a higher roled user")
+    def test_change_organisation(self):
+        updated_data = self.session.info.copy();
+        updated_data['organisationName'] = 'default'
+        res = self.session.request_path("PATCH", '/user/update', json=updated_data)
+        assert (res.ok) 
+    
+    def test_refreshToken(self):
+        res = self.session.request_path("GET", '/user/refreshToken')
+        assert (res.ok)
+        self.session.headers['Authorization'] = f'Bearer {res.json()["accessToken"]}'
+
+        res = self.session.request_path("GET", '/user/self')
+        assert (res.json()['user']['UUID']==self.session.info['UUID'])
+
+
+class TestOrganisationManager:
+    @pytest.fixture(autouse=True)
+    def _login(self):
+        self.su_session = commons.Session(credentials=commons.SUPERUSER_CREDENTIALS)
+        self.credentials = commons.generate_create_user_input(role=commons.Role.ORGANISATION_MANAGER)
+        res = self.su_session.request_path("POST", '/user/create', json=self.credentials)
+        self.session = commons.Session(credentials=self.credentials)
+        
+        yield None
+        
+        self.session.logout()
+        self.su_session.request_path("DELETE", '/user/delete', json=res.json()['user'])
+        self.su_session.logout()
+    
+    def test_get_users(self):
+        res = self.session.request_path("GET", '/user')
+        for e in res.json()['users']:
+            assert e['role']<self.session.info['role'], "Should only be able to query users with lower role"
+    
+    @pytest.mark.xfail(reason="Only Admins can get admin and super admin roled users")
+    def test_get_super_user(self):
+        res = self.session.request_path("GET", '/user/info/%s' % self.su_session.info['UUID'])
+        assert (res.ok)
+    
+    @pytest.mark.xfail(reason="Organisation manager cannot delete superadmin")
+    def test_delete_superuser(self):
+        res = self.session.request_path("DELETE", '/user/delete', json={'UUID': self.session.info['UUID']})
+        assert (res.ok), "Should fail"
+    
+    def test_change_password_normal(self):
+        res = self.session.request_path("PATCH", '/user/changePassword', json={'password':self.credentials['password'], 'newPassword':'12345678'})
+        assert (res.ok)
+    
+    @pytest.mark.xfail(reason="This method of changing password is only for admin roled user")
+    def test_change_password_admin(self):
+        res = self.session.request_path("PATCH", '/user/changePassword', json={'newPassword':'12345678'})
+        assert (res.ok)
+    
+    @pytest.mark.xfail(reason="Organisation manager cannot modify superadmin")
     def test_change_super_user_role(self):
         updated_data = self.su_session.info.copy();
         updated_data['role'] = 1
