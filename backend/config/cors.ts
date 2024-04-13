647a663360198cfd8415dd3a32abb090394ba58b
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-04-13 22:00:44 +0900

    Fixed cors and a backend/.env key

--- backend/config/cors.ts
+++ backend/config/cors.ts
@@ -1,13 +1,20 @@
 import { env } from "process"
+require('dotenv').config()
 
 let originList: any[] | string = '*' // allow all by default
-if (env.ROOT_DOMAIN !== undefined) {
-    const subdomainRe = new RegExp(`\.${env.ROOT_DOMAIN.replace(".", "\\.")}$`)
-    originList = [`http://${env.ROOT_DOMAIN}`, `https://${env.ROOT_DOMAIN}`, subdomainRe]
+if (env.ROOT_DOMAINS != undefined) {
+    originList = []
+    for (const domain of env.ROOT_DOMAINS.split(',')) {
+        if (domain) {
+            const subdomainRe = new RegExp(`\.${domain.replace(".", "\\.")}$`)
+            originList = [...originList, `http://${domain}`, `https://${domain}`, subdomainRe]
+        }
+    }
 }
 
 const corsConfig = { // ref: https://expressjs.com/en/resources/middleware/cors.html
     origin: originList,
+    credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
