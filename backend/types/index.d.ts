51f304bc0c75d3b069fd7a4d51291dab2dc42f76
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-04-14 14:41:12 +0900

    Updated backend configurations

--- backend/types/index.d.ts
+++ backend/types/index.d.ts
@@ -1,9 +1,9 @@
-import {Request as ExpressRequest, Response, NextFunction} from 'express'
+import {Request as ExpressRequest, Response as ExpressResponse, NextFunction as ExpressNextFunction} from 'express'
 
 export type User = {UUID: string, username: string, role: number, organisationId: string, recentlyLoggedIn?: boolean} // Without sensitive data.
 export type Request = ExpressRequest & {user?:User}
-export type Response = Response
-export type NextFunction = NextFunction
+export type Response = ExpressResponse
+export type NextFunction = ExpressNextFunction
 
 export type DefaultQuotaInput = {
     quotaTypeId: string, 
