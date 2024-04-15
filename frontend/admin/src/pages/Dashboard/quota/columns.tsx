395972cc02dbb280bcbe3b5b666d9ac4d0a7916e
Vin-Ren <65110648+Vin-Ren@users.noreply.github.com>
2024-04-15 15:06:57 +0900

    Added a new card in QuotaType details to filter quota by quota type and organisation

--- frontend/admin/src/pages/Dashboard/quota/columns.tsx
+++ frontend/admin/src/pages/Dashboard/quota/columns.tsx
@@ -1,5 +1,5 @@
 import { DataTableColumnHeader } from "@/components/data-table";
-import { Quota } from "@/lib/api/data-types";
+import { Quota, QuotaWithTicketOrganisationInfo, Ticket } from "@/lib/api/data-types";
 import { getGenericTableColumns } from "@/components/data-table-custom-columns";
 import { DataTableActionsCell } from "@/components/data-table-custom-columns/actions-cell";
 import { DataTableActionsHeader } from "@/components/data-table-custom-columns/actions-header";
@@ -25,6 +25,70 @@ export const getQuotaTableColumns = getGenericTableColumns<Quota>(
         {
             id: "Ticket Owner",
             accessorKey: "ticket.ownerName",
+            header: DataTableColumnHeader
+        },
+        {
+            id: "Usage Left",
+            accessorKey: "usageLeft",
+            header: DataTableColumnHeader
+        },
+        {
+            id: "actions",
+            header: ({ table }) => {
+                return (
+                    <DataTableActionsHeader
+                        table={table}
+                        actions={[
+                            QuotaHeaderDeleteAction()
+                        ]}
+                        {...actionsHeaderProps}
+                    />
+                )
+            },
+            cell: ({ row }) => {
+                return (
+                    <DataTableActionsCell
+                        row={row}
+                        actions={[
+                            QuotaViewDetailsAction(),
+                            QuotaViewQuotaTypeAction(),
+                            QuotaViewTicketAction(),
+                            QuotaEditAction(),
+                            QuotaDeleteAction()
+                        ]}
+                        {...actionsCellProps}
+                    />
+                )
+            },
+            enableSorting: false,
+            enableHiding: false,
+        },
+    ])
+)
+
+
+export const getQuotaFilteredByOrganisationAndQuotaTypeTableColumns = getGenericTableColumns<QuotaWithTicketOrganisationInfo>(
+    ({ actionsHeaderProps, actionsCellProps }) => ([
+        getDataTableSelectRowsColumn(),
+        {
+            id: "UUID",
+            accessorKey: "UUID",
+            header: DataTableColumnHeader,
+            cell: ({row}) => (<Link to={`/dashboard/quota/details/${row.original.UUID}`}>{row.original.UUID}</Link>)
+        },
+        {
+            id: "Ticket Owner",
+            accessorKey: "ticket.ownerName",
+            header: DataTableColumnHeader
+        },
+        {
+            id: "Quota Type",
+            accessorKey: "quotaType.name",
+            header: DataTableColumnHeader,
+        },
+        {
+            id: "Affiliated Organisation",
+            accessorKey: "ticket.ownerAffiliation.name",
             header: DataTableColumnHeader
         },
         {
